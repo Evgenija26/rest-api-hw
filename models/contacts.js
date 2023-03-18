4b876418cda6da7e7c0a4b1a43c1f1fb39b70201
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2023-03-18 09:37:48 +0100

    .

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,55 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsRath = path.join(__dirname, "./contacts.json")
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsRath, "utf-8");
+  const contacts = JSON.parse(data);
+  console.log("List of contacts: ");
+  console.table(contacts);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (id) => {    
+const data = await fs.readFile(contactsRath, "utf-8");
+const contacts = JSON.parse(data);
 
-const addContact = async (body) => {}
+const contact = contacts.find(item => item.id === id);
+console.log(`Get contact by ID ${id}:`);
+console.table(contact);
+return contact || null;}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const data = await fs.readFile(contactsRath, "utf-8");
+  const contacts = JSON.parse(data);
+
+  const newContact = contacts.filter((contact) => contact.id !== contactId);
+  console.log("Contact deleted successfully! New list of contacts: ");
+  console.table(newContact);
+}
+
+const addContact = async (name, email, phone) => {    
+const data = await fs.readFile(contactsRath, "utf-8");
+const contacts = JSON.parse(data);
+contacts.push({
+    id: contacts.length + 1,
+    name: name,
+    email: email,
+    phone: phone,
+  });
+
+  console.log(" New lists of contacts: ");
+  console.table(contacts);}
+
+const updateContact = async (id, data) => {
+  const contactsId = String(id);
+  const data = await fs.readFile(contactsRath, "utf-8");
+const contacts = JSON.parse(data);
+
+const contact = contacts.find(item => item.id === id);
+
+
+}
 
 module.exports = {
   listContacts,
